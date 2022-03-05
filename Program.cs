68266996b1a2182e85639f53bc67cdd485d1539c
kimberly <krodriguessilva53@gmail.com>
2022-03-04 22:24:35 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Cananéia");
+Console.WriteLine("Iguape");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Praia Grande");
